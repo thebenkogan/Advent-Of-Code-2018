94a1713eafad27df06a22fcb6126ca8c1b1bbe3d
Ben Kogan <benkogan9@gmail.com>
2023-12-27 01:46:48 -0500

    11, better

--- 11.py
+++ 11.py
@@ -1,11 +1,9 @@
-from collections import defaultdict
 from aoc import read_input
 
 lines = read_input()
 serial_number = int(lines[0])
 
-# (x, y, square size) -> power total
-sums = defaultdict(int)
+grid = [[0] * 300 for _ in range(300)]
 
 for x in range(1, 301):
     for y in range(1, 301):
@@ -15,29 +13,40 @@ for x in range(1, 301):
         power_level *= rack_id
         power_level = (power_level // 100) % 10
         power_level -= 5
-        sums[(x - 1, y - 1, 1)] = power_level
+        grid[y - 1][x - 1] = power_level
+
+# summed area table (I)
+# https://en.wikipedia.org/wiki/Summed-area_table
+sums = [[0] * 300 for _ in range(300)]
+for i in range(300):
+    for j in range(300):
+        total = grid[i][j]
+        if i > 0 and j > 0:
+            total += sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1]
+        elif i > 0:
+            total += sums[i - 1][j]
+        elif j > 0:
+            total += sums[i][j - 1]
+        sums[i][j] = total
 
 max_power = 0
-id = (0, 0)
-for size in range(2, 301):
+id = (0, 0, 0)
+for size in range(1, 300):
     for i in range(300 - size):
         for j in range(300 - size):
-            # imagine putting a one smaller square at this position
-            # it covers everything except one additional row and column
-            # we can place another one smaller square at (j+1,i+1) to catch those
-            # then there is a two smaller square at (j+1,i+1) that is double counted, so subtract that
-            # finally, add in the corners
-            total = (
-                sums[(j, i, size - 1)]
-                + sums[(j + 1, i + 1, size - 1)]
-                - sums[(j + 1, i + 1, size - 2)]
-                + sums[(j + size - 1, i, 1)]
-                + sums[(j, i + size - 1, 1)]
-            )
+            total = sums[i + size][j + size]
+            if i > 0 and j > 0:
+                total += (
+                    sums[i - 1][j - 1] - sums[i + size][j - 1] - sums[i - 1][j + size]
+                )
+            elif j > 0:
+                total -= sums[i + size][j - 1]
+            elif i > 0:
+                total -= sums[i - 1][j + size]
+
             if total > max_power:
                 max_power = total
-                id = (j + 1, i + 1, size)
-            sums[(j, i, size)] = total
+                id = (j + 1, i + 1, size + 1)
 
 
 print(id)
